555ecba7beca85c93a0d2cd8784da630126ebcb0
Muhammad-Omeiza <xyberhunter4cybercrime@gmail.com>
2022-09-09 17:22:26 -0700

    Completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-   /**
 
-    * main - Entry point
 
-    *
+/**
 
-    * Return: Always 0 (Success)
+ * main - Entry point
 
-    */
+ *
+
+ * Return: Always 0 (Success)
+
+ */
 
 int main(void)
-
+  
 {
   
-  char test_str[] = "\"Programming is like building a multilingual puzzle";
-  
-  puts(test_str);
+  puts("\"Programming is like building a multilingual puzzle");
   
   return (0);
   
