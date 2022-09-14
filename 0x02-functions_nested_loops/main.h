3bfb7b540f1709bf380ef0ebe8c2b2be0cbb364f
Muhammad-Omeiza <xyberhunter4cybercrime@gmail.com>
2022-09-14 02:16:50 -0700

    Task completed'

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,10 @@
-#include"main.h"
-/**
-* main: Entry point of the program
-*
-* Description: Simple program to print _putchar
-*
-* Return: Always 0
-*/
-int main (void)
-{
-char arr[] = "_putchar";
-int index;
-for (index = 0; index < 8; index++)
-{
-_putchar(arr[index]);
-}
-_putchar('\n');
-return (0);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif
