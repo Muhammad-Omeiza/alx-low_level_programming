9791d6638f3b255d238f3413e19771b75e88ac99
Muhammad-Omeiza <xyberhunter4cybercrime@gmail.com>
2022-09-14 04:40:48 -0700

    Task completed

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+* print_sign - a funtion to determine the sign of an integer
+*
+* @n: number of type int
+*
+* Return: 1 if it is positive, 0 if it is zero, and -1 if it is  negative
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
+}
