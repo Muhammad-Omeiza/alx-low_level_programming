1944f17b2ab057fe105cd6b8a3516249c28ba69e
Muhammad-Omeiza <xyberhunter4cybercrime@gmail.com>
2022-09-14 05:35:28 -0700

    Task completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+* add - funtion that add two integers
+*
+* @a: the first integer
+* @b: the second integer
+*
+* Return: a + b
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
